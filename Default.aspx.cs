b989ab5d84f9ab91835a2a5381e05f26b219c9a9
orzugg <orzugg@gmail.com>
2014-03-14 15:40:37 +0800

    qaz

--- Default.aspx.cs
+++ Default.aspx.cs
@@ -9,6 +9,6 @@ public partial class _Default : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-        Label1.Text = "Hello Wott!";
+        Label1.Text = "Hello Wotty!";
     }
 }
